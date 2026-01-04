a9ad95c2a3aa5b0bf453b8b364e32be313ea0755
wonst719 <wonst719@gmail.com>
2026-01-05 01:21:56 +0900

    npc.0000, 00d6, 12e0

--- C-OVL/NPC.C
+++ C-OVL/NPC.C
@@ -4,7 +4,117 @@
 
 #include <stdio.h>
 
-// npc
-void F_NPC_0000() { puts("F_NPC_0000"); }
-void F_NPC_00d6(int x) { printf("F_NPC_00d6(%d)\n", x); }
+// OK P1
+void F_NPC_0000(void)
+{
+    int local_4;
+    char* local_6;
+    byte local_26[32];
+    int local_28;
+
+#ifdef _WIN32
+    local_6 = NULL;
+#endif
+
+    D_5893_map_id--;
+
+    local_4 = (uint)D_5893_map_id >> 3;
+    switch (local_4)
+    {
+    case 0:
+        local_6 = "towne.npc";
+        break;
+    case 1:
+        local_6 = "dwelling.npc";
+        break;
+    case 2:
+        local_6 = "castle.npc";
+        break;
+    case 3:
+        local_6 = "keep.npc";
+        break;
+    }
+
+    local_28 = (uint)D_5893_map_id - (local_4 << 3);
+
+    FUN_1000_256e_read_file_from_disk(local_6, D_5d5e, 0x200, local_28 * 0x240);
+    FUN_1000_256e_read_file_from_disk(local_6, D_659e, 0x20, local_28 * 0x240 + 0x200);
+    FUN_1000_256e_read_file_from_disk(local_6, local_26, 0x20, local_28 * 0x240 + 0x220);
+
+    for (local_4 = 0; local_4 < 0x20; local_4++)
+    {
+        D_5f5e[local_4]._a = (u16)local_26[local_4];
+    }
+
+    D_5893_map_id++;
+}
+
+int F_NPC_12e0(int a, char b);
+
+// OK P1
+void F_NPC_00d6(int param_1)
+{
+    int local_4;
+    int local_6;
+
+    for (local_4 = 1; local_4 < 0x20; local_4++)
+    {
+        if (D_659e[local_4] == 0)
+        {
+            D_5f5e[local_4]._0 = 0;
+        }
+        else
+        {
+            local_6 = F_NPC_12e0(local_4, param_1);
+            D_5f5e[local_4]._0 = 1;
+
+            D_5f5e[local_4]._2 = D_5d5e[local_4]._3[local_6];
+            D_5f5e[local_4]._4 = D_5d5e[local_4]._6[local_6];
+            D_5f5e[local_4]._6 = D_5d5e[local_4]._9[local_6];
+
+            D_5f5e[local_4]._8 = D_659e[local_4];
+            D_5f5e[local_4]._e = local_6;
+            D_5f5e[local_4]._c = 0;
+
+            D_655e[local_4] = 0xffff;
+            D_615e[local_4].data[0] = 0;
+            D_65c2[local_4] = 0;
+        }
+    }
+}
+
 F_NPC_0db4() { puts("F_NPC_0db4"); }
+
+// F_NPC_12e0: optimized using assembly?
+// use literal conversion from FMT
+int F_NPC_12e0(int param_1, char param_2)
+{
+    int i;
+    int ret;
+    byte local_8[4];
+
+    for (i = 0; i < 4; i++)
+    {
+        local_8[i] = param_2 - D_5d5e[param_1]._c[i];
+    }
+
+    ret = 0;
+    if (local_8[1] < local_8[0])
+    {
+        local_8[0] = local_8[1];
+        ret = 1;
+    }
+
+    if (local_8[2] < local_8[0])
+    {
+        local_8[0] = local_8[2];
+        ret = 2;
+    }
+
+    if (local_8[3] < local_8[0])
+    {
+        ret = 1;
+    }
+
+    return ret;
+}
