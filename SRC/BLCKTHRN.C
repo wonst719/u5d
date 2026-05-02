6790c775c930170638b4e7b7d644de0eb5c31671
wonst719 <wonst719@gmail.com>
2026-05-02 21:26:26 +0900

    blckthrn 체크, 매칭

--- SRC/BLCKTHRN.C
+++ SRC/BLCKTHRN.C
@@ -10,17 +10,19 @@ void TOWN_11f0_Entry(int param_1);
 int CAST2_05e0(int a, int b);
 void CAST2_08ea(int a);
 
-// NOT MATCHING
+// OK P1
 void BLCKTHRN_0000(int param_1)
 {
-    for (; param_1 >= 0; param_1--)
+    int local_4;
+
+    for (local_4 = 0; local_4 < param_1; local_4++)
     {
         ULTIMA_433e_AudioSomeNoise();
         ULTIMA_3ae6(2);
     }
 }
 
-// NOT MATCHING
+// OK P1
 int BLCKTHRN_002e(int param_1)
 {
     int local_4;
@@ -54,11 +56,11 @@ int BLCKTHRN_002e(int param_1)
     case 0:
         D_5878--;
         break;
-    case 1:
-        D_5876++;
-        break;
     case 2:
         D_5878++;
+        break;
+    case 1:
+        D_5876++;
         break;
     case 3:
         D_5876--;
@@ -73,99 +75,114 @@ int BLCKTHRN_002e(int param_1)
 // NOT MATCHING
 void BLCKTHRN_00be(byte* param_1)
 {
-    byte* pbVar1;
-    int uVar2;
-    int uVar3;
-    byte bVar4;
-    bool bVar5;
-    bool bVar6;
-    int iVar7;
+    int local_4;
+    bool di;
+    bool local_a;
+    int local_e;
     int local_18;
     int local_14;
     int local_10;
     int local_6;
+    int local_12;
+    int local_16;
+    int local_8;
 
-    bVar5 = 1;
+    di = 1;
     while (1)
     {
-        local_6 = 1;
-        bVar6 = 0;
-        pbVar1 = param_1;
+        local_6 = 1; // a4c9
+        local_a = 0; // a4ce
 
         while (1)
         {
-            param_1 = pbVar1;
-            pbVar1 = param_1 + 1;
-            bVar4 = *param_1;
-            if (bVar4 == 0)
+            // a4d3 (default)
+            local_4 = *param_1++;
+            if (local_4 == 0)
             {
+                // -> a4fc
                 return;
             }
 
-            if (bVar4 > 0xf)
+            if (local_4 >= 0x10)
+            {
+                // -> a41c
                 break;
+            }
 
-            switch (bVar4)
+            // a4ec
+            switch (local_4)
             {
             case 1:
-                bVar6 = 1;
+                local_a = 1;
                 break;
+
             case 2:
-                local_6 = (uint)*pbVar1;
-                pbVar1 = param_1 + 2;
+                local_6 = (uint)*param_1;
+                param_1 += 2;
                 break;
+
             case 3:
-                bVar5 = 1;
+                di = 1;
                 break;
+
             case 4:
-                bVar5 = 0;
+                di = 0;
                 break;
+
             case 5:
-                ULTIMA_3ae6(*pbVar1);
-                pbVar1 = param_1 + 2;
+                ULTIMA_3ae6(*param_1);
+                param_1 += 2;
                 break;
+
             case 6:
-                D_ad14[param_1[2] + param_1[3] * 0x20] = *pbVar1;
-                pbVar1 = param_1 + 4;
+                D_ad14[param_1[2] + param_1[3] * 0x20] = *param_1;
+                param_1 += 4;
                 break;
+
             case 7:
                 ULTIMA_5910_UpdateFrame();
                 break;
+
             case 8:
                 BLCKTHRN_0000(local_6);
                 local_6 = 1;
                 break;
+
             case 9:
-                bVar4 = *pbVar1;
-                D_5c5a[bVar4]._1 = 0;
-                D_5c5a[bVar4]._0_tile = 0;
-                pbVar1 = param_1 + 2;
+                local_4 = *param_1;
+                D_5c5a[local_4]._1 = 0;
+                D_5c5a[local_4]._0_tile = 0;
+                param_1 += 2;
+                break;
             }
         }
 
-        iVar7 = BLCKTHRN_002e(bVar4);
-        uVar2 = D_5876;
-        uVar3 = D_5878;
-        if (bVar6)
+        // a41c
+        local_e = BLCKTHRN_002e(local_4);
+        local_12 = D_5876;
+        local_16 = D_5878;
+        if (local_a)
         {
-            local_10 = BLCKTHRN_002e(*pbVar1);
+            local_10 = BLCKTHRN_002e(*param_1);
             local_14 = D_5876;
             local_18 = D_5878;
-            pbVar1 = param_1 + 2;
+            param_1 += 2;
         }
 
-        param_1 = pbVar1;
-
-        for (; local_6 >= 0; local_6--)
+        // a454 ->
+        local_8 = 0;
+        for (; local_6 > 0; local_6--)
         {
-            D_5c5a[iVar7]._2_x += uVar2;
-            D_5c5a[iVar7]._3_y += uVar3;
-            if (bVar6)
+            // a495
+            D_5c5a[local_e]._2_x += local_12;
+            D_5c5a[local_e]._3_y += local_16;
+            if (local_a)
             {
                 D_5c5a[local_10]._2_x += local_14;
                 D_5c5a[local_10]._3_y += local_18;
             }
-            if (bVar5)
+
+            if (di)
             {
                 BLCKTHRN_0000(1);
             }
@@ -173,7 +190,7 @@ void BLCKTHRN_00be(byte* param_1)
     }
 }
 
-// NOT MATCHING
+// OK P1
 void BLCKTHRN_0278(int param_1, int param_2)
 {
     switch (param_1)
@@ -199,13 +216,11 @@ void BLCKTHRN_0278(int param_1, int param_2)
     }
 }
 
-// NOT MATCHING
+// OK P1
 int BLCKTHRN_02ea(int param_1)
 {
-    int iVar1;
-    int iVar2;
-    int iVar3;
-    int iVar4;
+    int local_1c;
+    int local_1a;
     int local_18;
     int local_16;
     char local_14[18];
@@ -213,18 +228,16 @@ int BLCKTHRN_02ea(int param_1)
     ULTIMA_1850_PrintString(/*0x6f7a*/ "\n\nYour response?\n:");
     ULTIMA_3b1c_GetString(local_14, 0xe);
     ULTIMA_1850_PrintString(/*0x6f8e*/ "\n\n");
-    iVar1 = ULTIMA_216c_strlen(D_1f5e[param_1]);
+    local_1c = ULTIMA_216c_strlen(D_1f5e[param_1]);
     local_16 = 0;
 
-    for (local_18 = 0; local_18 <= ULTIMA_216c_strlen(local_14) - iVar1; local_18++)
+    for (local_18 = 0; local_18 <= ULTIMA_216c_strlen(local_14) - local_1c; local_18++)
     {
         local_16 = 1;
 
-        for (iVar2 = 0; iVar2 < iVar1; iVar2++)
+        for (local_1a = 0; local_1a < local_1c; local_1a++)
         {
-            iVar3 = ULTIMA_2032_ToUpper((D_1f5e)[param_1][iVar2]);
-            iVar4 = ULTIMA_2032_ToUpper(local_14[local_18 + iVar2]);
-            if (iVar4 != iVar3)
+            if (ULTIMA_2032_ToUpper((byte)local_14[local_18 + local_1a]) != ULTIMA_2032_ToUpper((byte)D_1f5e[param_1][local_1a]))
             {
                 local_16 = 0;
                 break;
@@ -233,63 +246,70 @@ int BLCKTHRN_02ea(int param_1)
 
         if (local_16 != 0)
         {
-            return local_16;
+            break;
         }
     }
 
     return local_16;
 }
 
-// NOT MATCHING
+// CHECKED
 void BLCKTHRN_03ae(int param_1)
 {
-    int iVar6;
-    uint local_28;
+    int local_24;
+    int local_26;
+    int local_28;
     S_55a8 local_22;
 
-    if (param_1 == 0)
+    if (param_1 != 0)
     {
-        ULTIMA_1850_PrintString((char*)&D_b21e[0x12d]);
+        ULTIMA_1850_PrintString((char*)&D_b21e[0xf1]);
     }
     else
     {
-        ULTIMA_1850_PrintString((char*)&D_b21e[0xf1]);
+        ULTIMA_1850_PrintString((char*)&D_b21e[0x12d]);
     }
 
     ULTIMA_3ae6(10);
 
-    for (iVar6 = 2000; iVar6 < 25000; iVar6 += 0x32)
+    for (local_24 = 2000; local_24 < 25000; local_24 += 0x32)
     {
-        ULTIMA_2192_AudioSomeNoise(0xa50, 1, 200, iVar6, 0);
+        ULTIMA_2192_AudioSomeNoise(0xa50, 1, 200, local_24, 0);
     }
 
-    for (iVar6 = 25000; 2000 < iVar6; iVar6 -= 0x32)
+    for (local_24 = 25000; 2000 < local_24; local_24 -= 0x32)
     {
-        ULTIMA_2192_AudioSomeNoise(0xa50, 1, 200, iVar6, 0);
+        ULTIMA_2192_AudioSomeNoise(0xa50, 1, 200, local_24, 0);
     }
 
     ULTIMA_3522(D_5c5a[1]._2_x, D_5c5a[1]._3_y);
-    D_5c5a[1]._1 = 0;
-    D_5c5a[1]._0_tile = 0;
+
+    D_5c5a[1]._0_tile = D_5c5a[1]._1 = 0;
     D_ad14[0xe5] = 0x80;
 
+    local_26 = 0;
     for (local_28 = 0; local_28 < D_585b; local_28++)
     {
         if (D_55a8_party[local_28]._b != 'D')
         {
-            iVar6 = iVar6 + 1;
+            local_26++;
         }
-        if (iVar6 == 2)
+        if (local_26 == 2)
             break;
     }
 
+    // a6fc
     local_22 = D_55a8_party[local_28];
 
+    // NOT MATCHING
+    local_24 = local_28;
     for (; local_28 < 0xf; local_28++)
     {
-        D_55a8_party[local_28 + 1] = D_55a8_party[local_28];
+        // a73b
+        D_55a8_party[local_28] = D_55a8_party[local_28 + 1];
     }
 
+    // a752
     D_55a8_party[0xf] = local_22;
     D_55a8_party[0xf]._1f = 0x7f;
 
@@ -307,16 +327,15 @@ void BLCKTHRN_03ae(int param_1)
     ULTIMA_16ba_PrintChar(10);
 }
 
-// NOT MATCHING
+// OK P1
 void BLCKTHRN_0510(void)
 {
     ULTIMA_266c_GetChar();
     BLCKTHRN_00be(D_369e);
 }
 
-// NOT MATCHING
+// OK P1
 void BLCKTHRN_051c(void)
-
 {
     ULTIMA_1850_PrintString((char*)&D_b21e[0x229]);
     BLCKTHRN_00be(D_36da);
@@ -327,18 +346,18 @@ void BLCKTHRN_051c(void)
     ULTIMA_1850_PrintString(/*0x6fb4*/ "\n\n");
 }
 
-// NOT MATCHING
+// OK P1
 void BLCKTHRN_054a(int param_1, int param_2)
 {
-    bool bVar1;
-    int iVar4;
+    bool local_6;
+    int local_4;
 
-    bVar1 = 0;
+    local_6 = 0;
     ULTIMA_1850_PrintString(/*0x6fb8*/ "\n\n");
 
-    for (iVar4 = 0; iVar4 < 4; iVar4++)
+    for (local_4 = 0; local_4 < 4; local_4++)
     {
-        BLCKTHRN_0278(iVar4, param_1);
+        BLCKTHRN_0278(local_4, param_1);
         if (BLCKTHRN_02ea(param_1) != 0)
         {
             *(D_58d8 + param_1) = 0xff;
@@ -347,27 +366,28 @@ void BLCKTHRN_054a(int param_1, int param_2)
             {
                 BLCKTHRN_03ae(0);
                 BLCKTHRN_0510();
-                return;
             }
-
-            ULTIMA_1850_PrintString((char*)&D_b21e[0x29c]);
-            BLCKTHRN_0510();
-            return;
+            else
+            {
+                ULTIMA_1850_PrintString((char*)&D_b21e[0x29c]);
+                BLCKTHRN_0510();
+            }
+            break;
         }
 
         if (param_2 < 2)
         {
             ULTIMA_1850_PrintString((char*)&D_b21e[0x2dc]);
             BLCKTHRN_0510();
-            return;
+            break;
         }
 
-        if (bVar1)
+        if (local_6)
         {
             ULTIMA_4f7c(2);
             ULTIMA_2900_UpdateVitalsDisplay();
 
-            switch (iVar4)
+            switch (local_4)
             {
             case 0:
                 D_ad14[0x125] = 0xea;
@@ -385,26 +405,28 @@ void BLCKTHRN_054a(int param_1, int param_2)
         }
         else
         {
-            bVar1 = 1;
+            local_6 = 1;
             BLCKTHRN_051c();
         }
     }
 }
 
+// OK P1
 void BLCKTHRN_060e_Capture(void)
 {
-    undefined1 uVar3;
-    int iVar4;
-    uint uVar6;
-    int iVar7;
-    int iVar9;
-    int iVar10;
+    int local_8;
+    int local_e;
+    int local_c;
     int local_a;
+    int local_6;
+
+    int local_10;
+    ActorFmt* local_4;
 
     local_a = 0;
-    for (uVar6 = 0; uVar6 < D_585b; uVar6++)
+    for (local_8 = 0; local_8 < D_585b; local_8++)
     {
-        if (D_55a8_party[uVar6]._b != 'D')
+        if (D_55a8_party[local_8]._b != 'D')
         {
             local_a = local_a + 1;
         }
@@ -414,14 +436,16 @@ void BLCKTHRN_060e_Capture(void)
     ULTIMA_5910_UpdateFrame();
     ULTIMA_1850_PrintString(/*0x6fbc*/ "\nThou art subdued and blindfolded!");
 
-    for (iVar9 = 0; iVar9 < 8; iVar9++)
+    for (local_c = 0; local_c < 8; local_c++)
     {
-        if (D_58d8[iVar9] == 0)
+        if (D_58d8[local_c] == 0)
             break;
     }
 
-    if (iVar9 <= 7)
+    if (local_c < 8)
     {
+        local_10 = 0;
+
         ULTIMA_3ae6(2);
         ULTIMA_0a70_GRAP_2d_SetPenColor(0);
         ULTIMA_0aa6_GRAP_3f_FillRect(8, 8, 0xb7, 0xb7);
@@ -429,7 +453,7 @@ void BLCKTHRN_060e_Capture(void)
         ULTIMA_4a84();
         ULTIMA_2e96_SetWindDirection(-1);
 
-        for (iVar10 = 0; iVar10 < 5; iVar10++)
+        for (local_8 = 0; local_8 < 5; local_8++)
         {
             ULTIMA_20fa_WaitTicks(5);
             ULTIMA_433e_AudioSomeNoise();
@@ -437,29 +461,15 @@ void BLCKTHRN_060e_Capture(void)
 
         ULTIMA_1850_PrintString(/*0x6fe0*/ "\n\nStrong guards drag thee away!");
 
-        iVar10 = 0;
-        while (1)
+        for (local_8 = 0; local_8 < (D_a9ce != 0 ? 0x12 : 3); local_8++)
         {
-            if (D_a9ce == 0)
-            {
-                iVar4 = 3;
-            }
-            else
-            {
-                iVar4 = 0x12;
-            }
-
-            if (iVar4 <= iVar10)
-                break;
-
             ULTIMA_20fa_WaitTicks(5);
             ULTIMA_433e_AudioSomeNoise();
-            iVar10 = iVar10 + 1;
         }
 
-        for (iVar10 = 0; iVar10 < 0x20; iVar10++)
+        for (local_8 = 0; local_8 < 0x20; local_8++)
         {
-            ULTIMA_3a74(0, 0, 0, 0, 0, 0, iVar10);
+            ULTIMA_3a74(0, 0, 0, 0, 0, 0, local_8);
         }
 
         D_5893_map_id = 0xff;
@@ -468,28 +478,26 @@ void BLCKTHRN_060e_Capture(void)
         ULTIMA_256e_ReadFile(/*0x7000*/ "MISCMAPS.DAT", D_ac64, 0xb0, 0);
         ULTIMA_256e_ReadFile(/*0x700e*/ "MISCMSG.DAT", D_b21e, 1000, 0);
 
-        for (iVar4 = 0; iVar4 < 0xb; iVar4 = iVar4 + 1)
+        for (local_8 = 0; local_8 < 0xb; local_8++)
         {
-            for (iVar7 = 0; iVar7 < 0xb; iVar7 = iVar7 + 1)
+            for (local_e = 0; local_e < 0xb; local_e++)
             {
-                D_ad14[iVar4 * 0x20 + iVar7] = D_ac64[iVar4 * 0x10 + iVar7];
+                D_ad14[local_8 * 0x20 + local_e] = D_ac64[local_8 * 0x10 + local_e];
             }
         }
 
-        if (local_a > 0)
+        // a9ef
+        for (local_8 = 0; local_8 < local_a; local_8++)
         {
-            for (iVar10 = 0; iVar10 < local_a; iVar10++)
-            {
-                uVar6 = D_1f12[(local_a - 1) * 8 + iVar10];
-                D_5c5a[iVar10]._2_x = D_1f42[uVar6];
-                D_5c5a[iVar10]._3_y = D_1f48[uVar6];
-                iVar4 = ULTIMA_4d76(/*0x701a*/ "AMBFDTPRS", D_55a8_party[iVar10]._a);
-                uVar3 = D_1ade[iVar4];
-                D_5c5a[iVar10]._1 = uVar3;
-                D_5c5a[iVar10]._0_tile = uVar3;
-            }
+            local_4 = &D_5c5a[local_8];
+            local_4->_2_x = D_1f42[D_1f12[(local_a - 1) * 8 + local_8]];
+            local_4->_3_y = D_1f48[D_1f12[(local_a - 1) * 8 + local_8]];
+
+            local_6 = ULTIMA_4d76(/*0x701a*/ "AMBFDTPRS", D_55a8_party[local_8]._a);
+            local_4->_0_tile = local_4->_1 = D_1ade[local_6];
         }
 
+        // aa61
         ULTIMA_3ae6(0x10);
         ULTIMA_1850_PrintString(/*0x7024*/ "\n\nThou hast been chained and manacled!");
         ULTIMA_3ae6(0x32);
@@ -520,7 +528,7 @@ void BLCKTHRN_060e_Capture(void)
         BLCKTHRN_00be(D_370e);
         ULTIMA_1850_PrintString((char*)&D_b21e[0x32c]);
         ULTIMA_266c_GetChar();
-        BLCKTHRN_054a(iVar9, local_a);
+        BLCKTHRN_054a(local_c, local_a);
         if (D_5c5a[8]._0_tile != 0)
         {
             BLCKTHRN_00be(D_3716);
@@ -536,12 +544,14 @@ void BLCKTHRN_060e_Capture(void)
     D_5893_map_id = 0x12;
 }
 
-// NOT MATCHING
+// CHECKED
 void BLCKTHRN_0910_Death(void)
 {
     int local_c;
     int local_6;
     uint local_8;
+    int local_a;
+    int local_4;
 
     ULTIMA_251e_SwitchDisks(1);
 
@@ -568,7 +578,10 @@ void BLCKTHRN_0910_Death(void)
 
     for (local_8 = 0; local_8 < 0xb; local_8++)
     {
-        memset(&D_ad14[local_8 * 0x20], 0xff, 0xb);
+        for (local_a = 0; local_a < 0xb; local_a++)
+        {
+            D_ad14[local_8 * 0x20 + local_a] = 0xff;
+        }
     }
 
     for (local_8 = 0; local_8 < 0x20; local_8++)
@@ -585,13 +598,13 @@ void BLCKTHRN_0910_Death(void)
     ULTIMA_1850_PrintString("\n\nBut thy slumber is disturbed!");
 
     D_ad14[0xa5] = 0; // 0xadb9
-    D_5c5a[0]._1 = 0x1c;
-    D_5c5a[0]._0_tile = 0x1c;
-    D_5c5a[0]._3_y = 5;
-    D_5c5a[0]._2_x = 5;
+
+    D_5c5a[0]._0_tile = D_5c5a[0]._1 = 0x1c;
+    D_5c5a[0]._2_x = D_5c5a[0]._3_y = 5;
+
     ULTIMA_5910_UpdateFrame();
 
-    // TODO: match variable
+    // ac9d: NOT MATCHING
     for (local_8 = 0; local_8 < 6; local_8++)
     {
         ULTIMA_2192_AudioSomeNoise(D_3720[local_8], 1, D_372c[local_8], D_3738[local_8], D_3744[local_8]);
@@ -599,8 +612,8 @@ void BLCKTHRN_0910_Death(void)
 
     ULTIMA_1850_PrintString("\n\nSomeone shouts\n\n\"FORTIS FORTUNA\nAVENTARI\"");
     ULTIMA_20fa_WaitTicks(6);
-    D_5c5a[1]._1 = 0x16;
-    D_5c5a[1]._0_tile = 0x16;
+
+    D_5c5a[1]._0_tile = D_5c5a[1]._1 = 0x16;
     D_5c5a[1]._2_x = 2;
     D_5c5a[1]._3_y = 7;
     D_ad14[0xe2] = 0;
@@ -623,35 +636,30 @@ void BLCKTHRN_0910_Death(void)
     ULTIMA_3072();
     ULTIMA_3072();
     D_ad14[0x45] = 0;
-    D_5c5a[1]._1 = 0x16;
-    D_5c5a[1]._0_tile = 0x16;
+    D_5c5a[1]._0_tile = D_5c5a[1]._1 = 0x16;
     D_5c5a[1]._2_x = 5;
     D_5c5a[1]._3_y = 2;
     ULTIMA_1068(0x174, 5, 2);
-    D_5c5a[1]._1 = 0x74;
-    D_5c5a[1]._0_tile = 0x74;
+    D_5c5a[1]._0_tile = D_5c5a[1]._1 = 0x74;
     ULTIMA_5910_UpdateFrame();
 
+    // ad93
+    local_4 = D_5888 / 0x14;
     ULTIMA_1850_PrintString("\n\"");
-    ULTIMA_256e_ReadFile("KARMA.DAT", D_b21e, 2000, D_1a74[D_5888 / 0x14]);
+    ULTIMA_256e_ReadFile("KARMA.DAT", D_b21e, 2000, D_1a74[local_4]);
     ULTIMA_1850_PrintString((char*)D_b21e);
     ULTIMA_16ba_PrintChar(0x22);
     ULTIMA_266c_GetChar();
     ULTIMA_1850_PrintString("\n\nStrange words are intoned.");
     ULTIMA_20fa_WaitTicks(4);
 
-    if (D_585b != 0)
+    for (local_8 = 0; local_8 < D_585b; local_8++)
     {
-        for (local_8 = 0; local_8 < D_585b; local_8++)
-        {
-            // 32bit div?
-            // TODO: MATCH
-            //uVar5 = ULTIMA_03a0(0, uVar6 + 7, 0, 0x8e30);
-            ULTIMA_2192_AudioSomeNoise(0x8e30 / (local_8 + 7), 1, 30000, 2000, 2);
-            CAST2_05e0(local_8, 0xff);
-            D_55a8_party[local_8]._10 = D_55a8_party[local_8]._12;
-            ULTIMA_2900_UpdateVitalsDisplay();
-        }
+        local_6 = (u32)36400 / (local_8 + 7);
+        ULTIMA_2192_AudioSomeNoise(local_6, 1, 30000, 2000, 2);
+        CAST2_05e0(local_8, 0xff);
+        D_55a8_party[local_8]._10 = D_55a8_party[local_8]._12;
+        ULTIMA_2900_UpdateVitalsDisplay();
     }
 
     ULTIMA_1850_PrintString("\n\nVertigo...\n");
@@ -662,27 +670,31 @@ void BLCKTHRN_0910_Death(void)
     ULTIMA_10e0_GRAP_51_PutTile(0x11c, 5, 5);
     ULTIMA_0c22_GRAP_0f_SelectPage(0);
     ULTIMA_0f46_GRAP_66_Reveal(8, 8, 0xb7, 0xb7);
+
     if (D_5888 < 0x4b)
     {
         D_5888 = 0x4b;
     }
+
     D_5893_map_id = 0x11;
     D_5895_map_level = 1;
     D_587c = 0x1c;
-    D_5897_map_y = 10;
-    D_5896_map_x = 10;
-    D_588e = 0;
-    D_587a = 0;
+
+    D_5896_map_x = D_5897_map_y = 10;
+    D_587a = D_588e = 0;
+
     while (D_587f != 0x6)
     {
         ULTIMA_4f7c(9);
     }
-    D_58a6 = 0;
-    D_58a7 = 0;
+
+    D_58a7 = D_58a6 = 0;
+
     if (D_57a8 == 0)
     {
         D_57a8 = 0x3f;
     }
+
     if (local_c != 0 && local_c < 0x21)
     {
         TOWN_11f0_Entry(1);
