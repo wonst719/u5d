67178d34ea4a01336053442d100bbc94a34d3366
wonst719 <wonst719@gmail.com>
2026-05-04 14:32:18 +0900

    오디오 드라이버 분리

---
+++ SRC/AUD_DRV.C
@@ -0,0 +1,112 @@
+#include "AUD_DRV.H"
+#include "COMMON.H"
+
+#include <stdio.h>
+
+// TODO: clean up
+
+#if defined(TARGET_WINDOWS)
+#include <SDL3_mixer/SDL_mixer.h>
+#endif
+
+static MIX_Mixer* s_mixer;
+
+static MIX_Track* s_bgmTrack;
+static MIX_Track* s_sfxTrack;
+
+static SDL_PropertiesID s_bgmTrackProp;
+
+static SDL_AudioSpec s_mixerSpec;
+
+static MIX_Audio* s_bgm[20];
+static MIX_Audio* s_sfx[20];
+
+static int s_currentBgmId;
+
+void AUDIO_LoadBgmTable(void);
+void AUDIO_LoadSfxTable(void);
+
+void AUDIO_Init(void)
+{
+    MIX_Init();
+    s_mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
+    MIX_GetMixerFormat(s_mixer, &s_mixerSpec);
+
+    s_bgmTrack = MIX_CreateTrack(s_mixer);
+    s_sfxTrack = MIX_CreateTrack(s_mixer);
+
+    MIX_SetTrackGain(s_bgmTrack, 0.3f);
+    MIX_SetTrackGain(s_sfxTrack, 0.2f);
+
+    s_bgmTrackProp = SDL_CreateProperties();
+    SDL_SetNumberProperty(s_bgmTrackProp, MIX_PROP_PLAY_LOOPS_NUMBER, -1);
+
+    AUDIO_LoadBgmTable();
+    AUDIO_LoadSfxTable();
+}
+
+void AUDIO_Cleanup(void)
+{
+    // TODO: release audios
+
+    MIX_DestroyTrack(s_bgmTrack);
+    MIX_DestroyTrack(s_sfxTrack);
+    MIX_DestroyMixer(s_mixer);
+    s_mixer = NULL;
+    MIX_Quit();
+}
+
+void AUDIO_LoadBgmTable(void)
+{
+    s_bgm[1] = MIX_LoadAudio(s_mixer, "BGM/01.ogg", FALSE);
+    s_bgm[9] = MIX_LoadAudio(s_mixer, "BGM/09.ogg", FALSE);
+    const char* err = SDL_GetError();
+    if (err) { puts(err); }
+}
+
+void AUDIO_LoadSfxTable(void)
+{
+    s_sfx[SFX_ID_FOOTSTEP] = MIX_LoadAudio(s_mixer, "SFX/walk.wav", TRUE);
+    s_sfx[SFX_ID_BLOCKED] = MIX_LoadAudio(s_mixer, "SFX/blocked.wav", TRUE);
+    s_sfx[SFX_ID_POISON] = MIX_LoadAudio(s_mixer, "SFX/poison.wav", TRUE);
+    //s_sfx[SFX_ID_FOUNTAIN] = MIX_LoadAudio(s_mixer, "SFX/fountain.wav", TRUE);
+}
+
+void AUDIO_PlaySfx(int id)
+{
+    MIX_SetTrackAudio(s_sfxTrack, s_sfx[id]);
+    MIX_PlayTrack(s_sfxTrack, 0);
+    const char* err = SDL_GetError();
+    if (err) { puts(err); }
+}
+
+void AUDIO_StopSfx(void)
+{
+    Sint64 frames = MIX_MSToFrames(s_mixerSpec.freq, 10);
+    MIX_StopTrack(s_sfxTrack, frames);
+    const char* err = SDL_GetError();
+    if (err) { puts(err); }
+}
+
+void AUDIO_PlayBgm(int id)
+{
+    if (s_currentBgmId == id)
+        return;
+
+    s_currentBgmId = id;
+
+    MIX_SetTrackAudio(s_bgmTrack, s_bgm[id]);
+    MIX_PlayTrack(s_bgmTrack, s_bgmTrackProp);
+    const char* err = SDL_GetError();
+    if (err) { puts(err); }
+}
+
+void AUDIO_StopBgm(void)
+{
+    s_currentBgmId = 0;
+
+    Sint64 frames = MIX_MSToFrames(s_mixerSpec.freq, 1000);
+    MIX_StopTrack(s_bgmTrack, frames);
+    const char* err = SDL_GetError();
+    if (err) { puts(err); }
+}
--- SRC/AUD_SFX.C
+++ SRC/AUD_SFX.C
@@ -1,100 +1,7 @@
-#include <stdio.h>
-
-#include "AUD_DRV.H"
 #include "COMMON.H"
+#include "AUD_DRV.H"
 
-// TODO: clean up
-
-#if defined(TARGET_WINDOWS)
-#include <SDL3_mixer/SDL_mixer.h>
-#endif
-
-static MIX_Mixer* s_mixer;
-
-static MIX_Track* s_bgmTrack;
-static MIX_Track* s_sfxTrack;
-
-static SDL_AudioSpec s_mixerSpec;
-
-static MIX_Audio* s_bgm[20];
-static MIX_Audio* s_sfx[20];
-
-static int s_currentBgmId;
-
-void AUDIO_LoadBgmTable(void);
-void AUDIO_LoadSfxTable(void);
-
-void AUDIO_Init(void)
-{
-    MIX_Init();
-    s_mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
-    MIX_GetMixerFormat(s_mixer, &s_mixerSpec);
-
-    s_bgmTrack = MIX_CreateTrack(s_mixer);
-    s_sfxTrack = MIX_CreateTrack(s_mixer);
-
-    MIX_SetTrackGain(s_bgmTrack, 0.3f);
-    MIX_SetTrackGain(s_sfxTrack, 0.3f);
-
-    AUDIO_LoadBgmTable();
-    AUDIO_LoadSfxTable();
-}
-
-void AUDIO_Cleanup(void)
-{
-    // TODO: release audios
-
-    MIX_DestroyTrack(s_bgmTrack);
-    MIX_DestroyTrack(s_sfxTrack);
-    MIX_DestroyMixer(s_mixer);
-    s_mixer = NULL;
-    MIX_Quit();
-}
-
-void AUDIO_LoadBgmTable(void)
-{
-    s_bgm[1] = MIX_LoadAudio(s_mixer, "BGM/01.ogg", FALSE);
-    s_bgm[9] = MIX_LoadAudio(s_mixer, "BGM/09.ogg", FALSE);
-    const char* err = SDL_GetError();
-    if (err) { puts(err); }
-}
-
-void AUDIO_LoadSfxTable(void)
-{
-    s_sfx[0] = MIX_LoadAudio(s_mixer, "SFX/walk.wav", TRUE);
-    s_sfx[1] = MIX_LoadAudio(s_mixer, "SFX/blocked.wav", TRUE);
-}
-
-void AUDIO_PlaySfx(int id)
-{
-    MIX_SetTrackAudio(s_sfxTrack, s_sfx[id]);
-    MIX_PlayTrack(s_sfxTrack, 0);
-    const char* err = SDL_GetError();
-    if (err) { puts(err); }
-}
-
-void AUDIO_PlayBgm(int id)
-{
-    if (s_currentBgmId == id)
-        return;
-
-    s_currentBgmId = id;
-
-    MIX_SetTrackAudio(s_bgmTrack, s_bgm[id]);
-    MIX_PlayTrack(s_bgmTrack, 0);
-    const char* err = SDL_GetError();
-    if (err) { puts(err); }
-}
-
-void AUDIO_StopBgm(void)
-{
-    s_currentBgmId = 0;
-
-    Sint64 frames = MIX_MSToFrames(s_mixerSpec.freq, 1000);
-    MIX_StopTrack(s_bgmTrack, frames);
-    const char* err = SDL_GetError();
-    if (err) { puts(err); }
-}
+// TODO: add pc speaker emulation?
 
 // 2192
 void AUDIO_DispatchPulse(int freq, int delay, int dur, int pulseWidth, int pulseInc)
@@ -122,6 +29,31 @@ void AUDIO_DispatchWhiteNoise(uint rate, uint dur, uint limit)
     {
         s_footstepFlag = 0;
         AUDIO_PlaySfx(SFX_ID_FOOTSTEP);
+    }
+
+    if (rate == 10 && dur == 0x640 && limit == 2000)
+    {
+        AUDIO_PlaySfx(SFX_ID_POISON); // note: 60 (C4)
+    }
+    else if (rate == 0x14 && dur == 0x3c && limit == 10000)
+    {
+        AUDIO_PlaySfx(0x13); // note: 60 (C4)
+    }
+    else if (rate == 0x28 && dur == 3000 && limit == 500)
+    {
+        AUDIO_PlaySfx(7); // note: 60 (C4)
+    }
+    else if (rate == 10 && dur == 3000 && limit == 2000)
+    {
+        AUDIO_PlaySfx(8); // note: 60 (C4)
+    }
+    else if (rate == 800 && dur >= 8000 && dur <= 20800 && limit == 700)
+    {
+        AUDIO_PlaySfx(9); // note: 60 - (dur - 8000) / 1600
+    }
+    else if (rate == 10 && dur == 30 && limit == 25000)
+    {
+        AUDIO_PlaySfx(SFX_ID_FOUNTAIN);
     }
 }
 
