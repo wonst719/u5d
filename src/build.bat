9a0e7528500c50cee5f6155e1507d2c74bad5324
wonst719 <wonst719@gmail.com>
2026-05-09 00:04:28 +0900

    build.bat 되돌림

--- src/build.bat
+++ src/build.bat
@@ -19,42 +19,42 @@ echo START > log.txt
 
 del *.obj
 
-rem %CL% %CFLAGS% town.c >> log.txt
-rem %CL% %CFLAGS% mainout.c >> log.txt
-rem %CL% %CFLAGS% dungeon.c >> log.txt
-rem %CL% %CFLAGS% intro.c >> log.txt
-rem %CL% %CFLAGS% npc.c >> log.txt
-rem %CL% %CFLAGS% combat.c >> log.txt
-rem %CL% %CFLAGS% blckthrn.c >> log.txt
-rem %CL% %CFLAGS% lookobj.c >> log.txt
-rem %CL% %CFLAGS% dnglook.c >> log.txt
-rem %CL% %CFLAGS% outsubs.c >> log.txt
-rem %CL% %CFLAGS% shoppes.c >> log.txt
-rem %CL% %CFLAGS% endgame.c >> log.txt
-rem %CL% %CFLAGS% sjog.c >> log.txt
+%CL% %CFLAGS% town.c >> log.txt
+%CL% %CFLAGS% mainout.c >> log.txt
+%CL% %CFLAGS% dungeon.c >> log.txt
+%CL% %CFLAGS% intro.c >> log.txt
+%CL% %CFLAGS% npc.c >> log.txt
+%CL% %CFLAGS% combat.c >> log.txt
+%CL% %CFLAGS% blckthrn.c >> log.txt
+%CL% %CFLAGS% lookobj.c >> log.txt
+%CL% %CFLAGS% dnglook.c >> log.txt
+%CL% %CFLAGS% outsubs.c >> log.txt
+%CL% %CFLAGS% shoppes.c >> log.txt
+%CL% %CFLAGS% endgame.c >> log.txt
+%CL% %CFLAGS% sjog.c >> log.txt
 %CL% %CFLAGS% cmds.c >> log.txt
-rem %CL% %CFLAGS% cast.c >> log.txt
-rem %CL% %CFLAGS% talk.c >> log.txt
-rem %CL% %CFLAGS% cast2.c >> log.txt
-rem %CL% %CFLAGS% zstats.c >> log.txt
-rem %CL% %CFLAGS% comsubs.c >> log.txt
-rem %CL% %CFLAGS% shoppes2.c >> log.txt
-rem %CL% %CFLAGS% shoppes3.c >> log.txt
-rem %CL% %CFLAGS% font.c >> log.txt
-rem 
-rem %CL% %CFLAGS% vars.c >> log.txt
-rem %CL% %CFLAGS% file.c >> log.txt
-rem %CL% %CFLAGS_ULTIMA% ultima.c >> log.txt
-rem %CL% %CFLAGS% grap.c >> log.txt
-rem 
-rem %CL% %CFLAGS% 0000.c >> log.txt
-rem %CL% %CFLAGS% 1000.c >> log.txt
-rem %CL% %CFLAGS% 2000.c >> log.txt
-rem %CL% %CFLAGS% 3000.c >> log.txt
-rem %CL% %CFLAGS% 4000.c >> log.txt
-rem %CL% %CFLAGS% 5000.c >> log.txt
-rem %CL% %CFLAGS% 6000.c >> log.txt
-rem %CL% %CFLAGS% 7000.c >> log.txt
+%CL% %CFLAGS% cast.c >> log.txt
+%CL% %CFLAGS% talk.c >> log.txt
+%CL% %CFLAGS% cast2.c >> log.txt
+%CL% %CFLAGS% zstats.c >> log.txt
+%CL% %CFLAGS% comsubs.c >> log.txt
+%CL% %CFLAGS% shoppes2.c >> log.txt
+%CL% %CFLAGS% shoppes3.c >> log.txt
+%CL% %CFLAGS% font.c >> log.txt
+
+%CL% %CFLAGS% vars.c >> log.txt
+%CL% %CFLAGS% file.c >> log.txt
+%CL% %CFLAGS_ULTIMA% ultima.c >> log.txt
+%CL% %CFLAGS% grap.c >> log.txt
+
+%CL% %CFLAGS% 0000.c >> log.txt
+%CL% %CFLAGS% 1000.c >> log.txt
+%CL% %CFLAGS% 2000.c >> log.txt
+%CL% %CFLAGS% 3000.c >> log.txt
+%CL% %CFLAGS% 4000.c >> log.txt
+%CL% %CFLAGS% 5000.c >> log.txt
+%CL% %CFLAGS% 6000.c >> log.txt
+%CL% %CFLAGS% 7000.c >> log.txt
 
 rem %LINK% /MAP @u5.rsp >> log.txt
 
